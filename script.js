9f3231f7b6fa547ca792ccdad9e61fdc10e19b62
KHaled Refat <96064446+khaledrefat@users.noreply.github.com>
2023-02-22 04:12:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 window.addEventListener("load", function() {
 	document.querySelector("body").classList.add("loaded");
 });
+
+//send message with whatsapp
+
+function sendWhatsApp() {
+	var phoneNumber = '+201010612391';
+	var message = 'Hello, there!';
+	var whatsappLink = 'https://wa.me/' + phoneNumber + '?text=' + message;
+	if (navigator.userAgent.match(/Android/i)) {
+	  window.location.href = 'intent://' + whatsappLink + '#Intent;package=com.whatsapp;scheme=https;end';
+	} else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+	  window.location.href = 'whatsapp://send?phone=' + phoneNumber + '&text=' + message;
+	} else {
+	  window.location.href = whatsappLink;
+	}
+  }
+  
